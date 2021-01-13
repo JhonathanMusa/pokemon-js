ddead9bb6f53963ad7b4caf90b3ae9a29598f0ba
jhonathanmusa <jhonathansamu91@gmail.com>
2021-01-12 22:30:04 -0500

    changing some things

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/movies">Movies</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/movies">Movies</router-link> -->
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
